68101fa98144b36ac8648d5147642cfac86d4682
mwenda <brianmwenda255@gmail.com>
2022-12-14 21:24:43 +0300

    create fix

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * main - prints _putchar and a new line
- * Return: Always 0
+ * main -  prints _putchar, followed by a new line
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,17 +1,25 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - prints alpha 10 times
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
+ * followed by a new line
  */
 void print_alphabet_x10(void)
 {
-	int ten;
-	char la;
+	char ch;
+	int i;
 
-	for (ten = 0; ten <= 9; ten++)
+	i = 0;
+
+	while (i < 10)
 	{
-		for (la = 'a'; la <= 'z'; la++)
-			_putchar(la);
+		ch = 'a';
+		while (ch <= 'z')
+		{
+			_putchar(ch);
+			ch++;
+		}
 		_putchar('\n');
+		i++;
 	}
 }
